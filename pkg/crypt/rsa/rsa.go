130bb33a369a96a22bc9ce13e661b2b9a1df9508
penghuihui <phh2008@vip.qq.com>
2023-11-29 00:12:55 +0800

    1.增加私钥加密，公钥解密

--- pkg/crypt/rsa/rsa.go
+++ pkg/crypt/rsa/rsa.go
@@ -10,6 +10,7 @@ import (
 	"encoding/base64"
 	"encoding/pem"
 	"errors"
+	"math/big"
 	"runtime"
 )
 
@@ -106,6 +107,61 @@ func EncryptByPubKey(plainText []byte, pubKey []byte) (base64CipherText string, 
 // DecryptByPriKey 解密
 func DecryptByPriKey(cipherTextBytes []byte, priKey []byte) (plainText []byte, err error) {
 	return decryptByPrivateKey(cipherTextBytes, priKey)
+}
+
+// EncryptByPriKey 私钥加密（加密内容不能超过密钥长度）
+func EncryptByPriKey(plainText []byte, priKey []byte) (base64CipherText string, err error) {
+	// 获取私钥
+	block, _ := pem.Decode(priKey)
+	if block == nil {
+		return "", errors.New("key is invalid format")
+	}
+	// x509 parse
+	privateKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return "", err
+	}
+	pk := privateKey.(*rsa.PrivateKey)
+	// 签名
+	cipherBytes, err := rsa.SignPKCS1v15(rand.Reader, pk, crypto.Hash(0), plainText)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(cipherBytes), nil
+}
+
+// DecryptByPubKey 公钥解密
+func DecryptByPubKey(cipherBytes []byte, publicKey []byte) (plainText []byte, err error) {
+	// 获取公钥
+	block, _ := pem.Decode(publicKey)
+	if block == nil {
+		return nil, errors.New("key is invalid format")
+	}
+	pubKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	pk, ok := pubKey.(*rsa.PublicKey)
+	if !ok {
+		return nil, errors.New("the kind of key is not a rsa.PublicKey")
+	}
+	c := new(big.Int)
+	m := new(big.Int)
+	m.SetBytes(cipherBytes)
+	e := big.NewInt(int64(pk.E))
+	c.Exp(m, e, pk.N)
+	out := c.Bytes()
+	skip := 0
+	for i := 2; i < len(out); i++ {
+		if i+1 >= len(out) {
+			break
+		}
+		if out[i] == 0xff && out[i+1] == 0 {
+			skip = i + 2
+			break
+		}
+	}
+	return out[skip:], nil
 }
 
 // CreateSign RSA sign
--- pkg/crypt/rsa/rsa_test.go
+++ pkg/crypt/rsa/rsa_test.go
@@ -39,6 +39,35 @@ func TestRSAEncrypt(t *testing.T) {
 	fmt.Println("解密后：", string(ret))
 }
 
+func TestRSAEncrypt2(t *testing.T) {
+	priBuf := bytes.NewBuffer(nil)
+	err := GeneratePrivateKey(2048, priBuf)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("私钥：")
+	fmt.Println(string(priBuf.Bytes()))
+	pubBuf := bytes.NewBuffer(nil)
+	err = GeneratePublicKey(priBuf.Bytes(), pubBuf)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("公钥：")
+	fmt.Println(string(pubBuf.Bytes()))
+	var src = []byte("山不在高，有仙则名。水不在深，有龙则灵。斯是陋室，惟吾德馨。苔痕上阶绿，草色入帘青。谈笑有鸿儒，往来无白丁。")
+	enc, err := EncryptByPriKey(src, priBuf.Bytes())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("加密后：", enc)
+	val, _ := base64.StdEncoding.DecodeString(enc)
+	ret, err := DecryptByPubKey(val, pubBuf.Bytes())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("解密后：", string(ret))
+}
+
 func TestRSASign(t *testing.T) {
 	priBuf := bytes.NewBuffer(nil)
 	err := GeneratePrivateKey(2048, priBuf)
