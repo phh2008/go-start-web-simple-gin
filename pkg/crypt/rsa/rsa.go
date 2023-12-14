d7c530d9994ec6fb8987ffb9267f09185add47b3
penghuihui <huihui.peng@gientech.com>
2023-12-14 12:52:56 +0800

    update rsa

--- pkg/crypt/rsa/rsa.go
+++ pkg/crypt/rsa/rsa.go
@@ -1,214 +1,61 @@
 package rsa
 
 import (
-	"bytes"
-	"com.gientech/selection/pkg/logger"
 	"crypto"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/base64"
-	"encoding/pem"
-	"errors"
-	"math/big"
-	"runtime"
 )
 
-func encryptByPublicKey(plainText, publicKey []byte) (cipherText []byte, err error) {
-	defer func() {
-		if err := recover(); err != nil {
-			switch err.(type) {
-			case runtime.Error:
-				logger.Errorf("runtime err=%v,Check that the key or text is correct", err)
-			default:
-				logger.Errorf("error=%v,check the cipherText ", err)
-			}
-		}
-	}()
-	block, _ := pem.Decode(publicKey)
-	if block == nil {
-		return nil, errors.New("key is invalid format")
+// Encrypt encrypts by rsa with public key or private key.
+// 通过 rsa 公钥或私钥加密
+func Encrypt(rsaKey interface{}, src []byte) CodecResult {
+	keyPair := NewKeyPair()
+	keyPair.SetPublicKey(interface2bytes(rsaKey))
+	keyPair.SetPrivateKey(interface2bytes(rsaKey))
+	var c CodecResult
+	// 私钥加密
+	if keyPair.IsPrivateKey() {
+		c.Raw, c.Error = keyPair.EncryptByPrivateKey(src)
+		return c
 	}
-	pubKey, err := x509.ParsePKIXPublicKey(block.Bytes)
-	if err != nil {
-		return nil, err
-	}
-	pk, ok := pubKey.(*rsa.PublicKey)
-	if !ok {
-		return nil, errors.New("the kind of key is not a rsa.PublicKey")
-	}
-	pubSize, plainTextSize := pk.Size(), len(plainText)
-	offSet, once := 0, pubSize-11
-	buffer := bytes.Buffer{}
-	for offSet < plainTextSize {
-		endIndex := offSet + once
-		if endIndex > plainTextSize {
-			endIndex = plainTextSize
-		}
-		bytesOnce, err := rsa.EncryptPKCS1v15(rand.Reader, pk, plainText[offSet:endIndex])
-		if err != nil {
-			return nil, err
-		}
-		buffer.Write(bytesOnce)
-		offSet = endIndex
-	}
-	cipherText = buffer.Bytes()
-	return cipherText, nil
+	// 公钥加密
+	c.Raw, c.Error = keyPair.EncryptByPublicKey(src)
+	return c
 }
 
-func decryptByPrivateKey(cipherText, privateKey []byte) (plainText []byte, err error) {
-	defer func() {
-		if err := recover(); err != nil {
-			switch err.(type) {
-			case runtime.Error:
-				logger.Errorf("runtime err=%v,Check that the key or text is correct", err)
-			default:
-				logger.Errorf("error=%v,check the cipherText ", err)
-			}
-		}
-	}()
-	block, _ := pem.Decode(privateKey)
-	if block == nil {
-		return nil, errors.New("key is invalid format")
+// Decrypt decrypts by rsa with private key or public key.
+// 通过 rsa 私钥或公钥解密
+func Decrypt(rsaKey interface{}, src []byte) CodecResult {
+	keyPair := NewKeyPair()
+	keyPair.SetPublicKey(interface2bytes(rsaKey))
+	keyPair.SetPrivateKey(interface2bytes(rsaKey))
+	var p CodecResult
+	if keyPair.IsPublicKey() {
+		p.Raw, p.Error = keyPair.DecryptByPublicKey(src)
+		return p
 	}
-	pri, err := x509.ParsePKCS8PrivateKey(block.Bytes)
-	if err != nil {
-		return []byte{}, err
-	}
-	pk := pri.(*rsa.PrivateKey)
-	priSize, cipherTextSize := pk.Size(), len(cipherText)
-	var offSet = 0
-	var buffer = bytes.Buffer{}
-	for offSet < cipherTextSize {
-		endIndex := offSet + priSize
-		if endIndex > cipherTextSize {
-			endIndex = cipherTextSize
-		}
-		bytesOnce, err := rsa.DecryptPKCS1v15(rand.Reader, pk, cipherText[offSet:endIndex])
-		if err != nil {
-			return nil, err
-		}
-		buffer.Write(bytesOnce)
-		offSet = endIndex
-	}
-	plainText = buffer.Bytes()
-	return plainText, nil
+	p.Raw, p.Error = keyPair.DecryptByPrivateKey(src)
+	return p
 }
 
-// EncryptByPubKey 加密
-func EncryptByPubKey(plainText []byte, pubKey []byte) (base64CipherText string, err error) {
-	cipherBytes, err := encryptByPublicKey(plainText, pubKey)
-	if err != nil {
-		return "", err
-	}
-	return base64.StdEncoding.EncodeToString(cipherBytes), nil
+// CreateSign by rsa with private key.
+// 通过 rsa 私钥签名
+func CreateSign(privateKey interface{}, hash crypto.Hash, src []byte) CodecResult {
+	keyPair := NewKeyPair()
+	keyPair.SetPrivateKey(interface2bytes(privateKey))
+	keyPair.SetHash(hash)
+	var s CodecResult
+	s.Raw, s.Error = keyPair.SignByPrivateKey(src)
+	return s
 }
 
-// DecryptByPriKey 解密
-func DecryptByPriKey(cipherTextBytes []byte, priKey []byte) (plainText []byte, err error) {
-	return decryptByPrivateKey(cipherTextBytes, priKey)
-}
-
-// EncryptByPriKey 私钥加密（加密内容不能超过密钥长度）
-func EncryptByPriKey(plainText []byte, priKey []byte) (base64CipherText string, err error) {
-	// 获取私钥
-	block, _ := pem.Decode(priKey)
-	if block == nil {
-		return "", errors.New("key is invalid format")
+// VerifySign verify sign by rsa with public key.
+// 通过 rsa 公钥验签
+func VerifySign(publicKey interface{}, hash crypto.Hash, src []byte, sign []byte) (bool, error) {
+	if len(src) == 0 || len(sign) == 0 {
+		return false, nil
 	}
-	// x509 parse
-	privateKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
-	if err != nil {
-		return "", err
-	}
-	pk := privateKey.(*rsa.PrivateKey)
-	// 签名
-	cipherBytes, err := rsa.SignPKCS1v15(rand.Reader, pk, crypto.Hash(0), plainText)
-	if err != nil {
-		return "", err
-	}
-	return base64.StdEncoding.EncodeToString(cipherBytes), nil
-}
-
-// DecryptByPubKey 公钥解密
-func DecryptByPubKey(cipherBytes []byte, publicKey []byte) (plainText []byte, err error) {
-	// 获取公钥
-	block, _ := pem.Decode(publicKey)
-	if block == nil {
-		return nil, errors.New("key is invalid format")
-	}
-	pubKey, err := x509.ParsePKIXPublicKey(block.Bytes)
-	if err != nil {
-		return nil, err
-	}
-	pk, ok := pubKey.(*rsa.PublicKey)
-	if !ok {
-		return nil, errors.New("the kind of key is not a rsa.PublicKey")
-	}
-	c := new(big.Int)
-	m := new(big.Int)
-	m.SetBytes(cipherBytes)
-	e := big.NewInt(int64(pk.E))
-	c.Exp(m, e, pk.N)
-	out := c.Bytes()
-	skip := 0
-	for i := 2; i < len(out); i++ {
-		if i+1 >= len(out) {
-			break
-		}
-		if out[i] == 0xff && out[i+1] == 0 {
-			skip = i + 2
-			break
-		}
-	}
-	return out[skip:], nil
-}
-
-// CreateSign RSA sign
-func CreateSign(src []byte, priKey []byte, hash crypto.Hash) ([]byte, error) {
-	block, _ := pem.Decode(priKey)
-	if block == nil {
-		return nil, errors.New("key is invalid format")
-	}
-	// x509 parse
-	privateKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
-	if err != nil {
-		return nil, err
-	}
-	pk := privateKey.(*rsa.PrivateKey)
-	h := hash.New()
-	_, err = h.Write(src)
-	if err != nil {
-		return nil, err
-	}
-	bt := h.Sum(nil)
-	sign, err := rsa.SignPKCS1v15(rand.Reader, pk, hash, bt)
-	if err != nil {
-		return nil, err
-	}
-	return sign, nil
-}
-
-// VerifySign RSA verify
-func VerifySign(src, sign, pubKey []byte, hash crypto.Hash) error {
-	block, _ := pem.Decode(pubKey)
-	if block == nil {
-		return errors.New("key is invalid format")
-	}
-	// x509 parse
-	publicKeyInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
-	if err != nil {
-		return err
-	}
-	publicKey, ok := publicKeyInterface.(*rsa.PublicKey)
-	if !ok {
-		return errors.New("the kind of key is not a rsa.PublicKey")
-	}
-	h := hash.New()
-	_, err = h.Write(src)
-	if err != nil {
-		return err
-	}
-	bt := h.Sum(nil)
-	return rsa.VerifyPKCS1v15(publicKey, hash, bt, sign)
+	keyPair := NewKeyPair()
+	keyPair.SetPublicKey(interface2bytes(publicKey))
+	keyPair.SetHash(hash)
+	err := keyPair.VerifyByPublicKey(src, sign)
+	return err == nil, err
 }
--- pkg/crypt/rsa/rsa_test.go
+++ pkg/crypt/rsa/rsa_test.go
@@ -1,88 +1,52 @@
 package rsa
 
 import (
-	"bytes"
+	"com.gientech/selection/pkg/crypt/openssl"
 	"crypto"
-	"encoding/base64"
-	"fmt"
 	"github.com/stretchr/testify/assert"
-	"log"
 	"testing"
 )
 
-func TestRSAEncrypt(t *testing.T) {
-	priBuf := bytes.NewBuffer(nil)
-	err := GeneratePrivateKey(2048, priBuf)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println("私钥：")
-	fmt.Println(string(priBuf.Bytes()))
-	pubBuf := bytes.NewBuffer(nil)
-	err = GeneratePublicKey(priBuf.Bytes(), pubBuf)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println("公钥：")
-	fmt.Println(string(pubBuf.Bytes()))
-	var src = []byte("山不在高，有仙则名。水不在深，有龙则灵。斯是陋室，惟吾德馨。苔痕上阶绿，草色入帘青。谈笑有鸿儒，往来无白丁。")
-	enc, err := EncryptByPubKey(src, pubBuf.Bytes())
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("加密后：", enc)
-	val, _ := base64.StdEncoding.DecodeString(enc)
-	ret, err := DecryptByPriKey(val, priBuf.Bytes())
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("解密后：", string(ret))
+func TestRsa_EncryptWithPublicKey(t *testing.T) {
+	input := []byte("hello world!!!")
+	pubKey, priKey := openssl.RSA.GenKeyPair(openssl.PKCS8, 2048)
+	t.Log("公钥：\n", string(pubKey))
+	t.Log("私钥：\n", string(priKey))
+
+	enMsg := Encrypt(pubKey, input)
+	assert.Nil(t, enMsg.Error)
+	t.Log("加密后：", enMsg.ToBase64String())
+
+	deMsg := Decrypt(priKey, enMsg.Raw)
+	assert.Nil(t, deMsg.Error)
+	t.Log("解密后：", deMsg.String())
 }
 
-func TestRSAEncrypt2(t *testing.T) {
-	priBuf := bytes.NewBuffer(nil)
-	err := GeneratePrivateKey(2048, priBuf)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println("私钥：")
-	fmt.Println(string(priBuf.Bytes()))
-	pubBuf := bytes.NewBuffer(nil)
-	err = GeneratePublicKey(priBuf.Bytes(), pubBuf)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println("公钥：")
-	fmt.Println(string(pubBuf.Bytes()))
-	var src = []byte("山不在高，有仙则名。水不在深，有龙则灵。斯是陋室，惟吾德馨。苔痕上阶绿，草色入帘青。谈笑有鸿儒，往来无白丁。")
-	enc, err := EncryptByPriKey(src, priBuf.Bytes())
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("加密后：", enc)
-	val, _ := base64.StdEncoding.DecodeString(enc)
-	ret, err := DecryptByPubKey(val, pubBuf.Bytes())
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("解密后：", string(ret))
+func TestRsa_EncryptWithPrivateKey(t *testing.T) {
+	input := []byte("hello world!!!")
+	pubKey, priKey := openssl.RSA.GenKeyPair(openssl.PKCS8, 2048)
+	t.Log("公钥：\n", string(pubKey))
+	t.Log("私钥：\n", string(priKey))
+
+	enMsg := Encrypt(priKey, input)
+	assert.Nil(t, enMsg.Error)
+	t.Log("加密后：", enMsg.ToBase64String())
+
+	deMsg := Decrypt(pubKey, enMsg.Raw)
+	assert.Nil(t, deMsg.Error)
+	t.Log("解密后：", deMsg.String())
 }
 
-func TestRSASign(t *testing.T) {
-	priBuf := bytes.NewBuffer(nil)
-	err := GeneratePrivateKey(2048, priBuf)
-	t.Logf("private key: %s\n", priBuf.Bytes())
+func TestRsa_SignWithPrivateKy(t *testing.T) {
+	input := []byte("hello world!!!")
+	pubKey, priKey := openssl.RSA.GenKeyPair(openssl.PKCS8, 2048)
+	t.Log("公钥：\n", string(pubKey))
+	t.Log("私钥：\n", string(priKey))
 
-	pubBuf := bytes.NewBuffer(nil)
-	err = GeneratePublicKey(priBuf.Bytes(), pubBuf)
-	assert.NoError(t, err)
-	t.Logf("public key: %s\n", pubBuf.Bytes())
-
-	src := []byte("123456")
-	sign, err := CreateSign(src, priBuf.Bytes(), crypto.SHA256)
-	assert.NoError(t, err)
-	t.Logf("sign out: %s\n", base64.RawStdEncoding.EncodeToString(sign))
-
-	err = VerifySign(src, sign, pubBuf.Bytes(), crypto.SHA256)
-	assert.NoError(t, err)
+	signResult := CreateSign(priKey, crypto.SHA256, input)
+	t.Log("签名串：", signResult.ToBase64String())
+	assert.Nil(t, signResult.Error)
+	ok, err := VerifySign(pubKey, crypto.SHA256, input, signResult.Raw)
+	assert.Nil(t, err)
+	t.Log("签名验证是否通过：", ok)
 }
